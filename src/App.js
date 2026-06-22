4a3462bf8cae4c4a19a583c0a5665bfccfe505c2
bananchik-47 <akievkrytou47@gmail.com>
2026-06-22 19:30:23 +0300

    Добавил карты товаров, корзину и код добавления и удаления товаров из корзины

--- src/App.js
+++ src/App.js
@@ -1,31 +1,90 @@
 import React from "react";
 import Header from "./componens/Header";
 import Footer from "./componens/Footer";
+import Items from "./componens/Items";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      orders: [],
       items: [
-        { 
-          id: 1, 
-          title: "Товар 1",
-          img: "https://via.placeholder.com/150",
-          desc: "Описание товара 1",
-          category: "Категория 1",
-          price: 100 
+        {
+          id: 1,
+          title: "Стул серый",
+          img: "grey-chair.jpg",
+          desc: "Удобный серый стул для кухни и гостиной",
+          category: "chairs",
+          price: "49.99",
         },
-        
+        {
+          id: 2,
+          title: "Стул белый",
+          img: "white-chair.jpg",
+          desc: "Лаконичный белый стул в скандинавском стиле",
+          category: "chairs",
+          price: "45.99",
+        },
+        {
+          id: 3,
+          title: "Стул деревянный",
+          img: "chair-wood.jpg",
+          desc: "Классический деревянный стул из натурального дерева",
+          category: "chairs",
+          price: "59.99",
+        },
+        {
+          id: 4,
+          title: "Стол обеденный",
+          img: "table-dining.jpg",
+          desc: "Просторный обеденный стол для всей семьи",
+          category: "tables",
+          price: "129.99",
+        },
+        {
+          id: 5,
+          title: "Стол журнальный",
+          img: "table-coffee.jpg",
+          desc: "Компактный журнальный столик для гостиной",
+          category: "tables",
+          price: "79.99",
+        },
+        {
+          id: 6,
+          title: "Диван серый трёхместный",
+          img: "sofa-grey.jpg",
+          desc: "Мягкий серый диван с удобной посадкой",
+          category: "sofas",
+          price: "299.99",
+        },
       ],
     };
+    this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
-        <Header />
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Items items={this.state.items} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter(el => el.id !== id) });
+  }
+
+  addToOrder(item) {
+    let isInArray = false;
+    this.state.orders.forEach(el => {
+      if (el.id === item.id) 
+        isInArray = true;
+    })
+    if (!isInArray) {
+      this.setState({ orders: [...this.state.orders, item] });
+    }
   }
 }
 
